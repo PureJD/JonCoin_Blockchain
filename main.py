7ccaa7116021df56b11e2aea014db97c922a05a7
Jon <6454jonny@gmail.com>
2024-06-08 15:40:30 +0100

    default values added to inital block to prevent issues of no arugments being issued upon creation of object

--- main.py
+++ main.py
@@ -2,19 +2,16 @@ import hashlib
 
 class JonCoin:
 
-    def __init__(self, previous_block_hash, transaction_list):
+    def __init__(self, previous_block_hash = '0', transaction_list = 'First Block'):
+        '''This blockchain constructor which will create the first block'''
         self.previous_block_hash = previous_block_hash
         self.transaction_list = transaction_list
         self.block_data = "-".join(transaction_list) + "-" + previous_block_hash
         self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest() 
 
 
-t1 = "Anna sends 2 NC to Mike"
-t2 = "Dave sends 2 NC to Mike"
-t3 = "Simon sends 2 NC to Mike"
-t4 = "Paul sends 2 NC to Mike"
-t5 = "Anna sends 2 NC to Mike"
 
-inital_block = JonCoin('inital message', [t1, t2, t3, t4, t5])
+
+inital_block = JonCoin()
 print(inital_block.block_data)
 print(inital_block.block_hash)
